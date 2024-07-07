d2176f88de64a9777b03ea3f8c261c591d207b2b
Cameron Haynes <chaynes2019@gmail.com>
2024-07-06 21:33:32 -0400

    Modify initial data analysis script to run the pre-coding site generation part of the data analysis pipeline, allowing me to see the analyze output files

--- experiments/2022-3-29-PaperDuplication/AssociatedScripts/runInitialDataAnalysis.sh
+++ experiments/2022-3-29-PaperDuplication/AssociatedScripts/runInitialDataAnalysis.sh
@@ -1,16 +1,27 @@
+#!/bin/bash
+# The interpreter used to execute the script
+#SBATCH --job-name=broadTimecourseDuplicationDataAnalysis
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=clhaynes@umich.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=2g
+#SBATCH --time=00-08:00:00
+#SBATCH --account=zamanlh0
+#SBATCH --array=1-10
+
 USERNAME=clhaynes
 EXPERIMENT_ID=2022-3-29-PaperDuplication
 
 EXPERIMENT_DIR=/scratch/zamanlh_root/zamanlh0/${USERNAME}/${EXPERIMENT_ID}
 
-cp CodingSiteGeneratorHPCCCopy.py ${EXPERIMENT_DIR}
-
 echo "Experiment: ${EXPERIMENT_ID}"
-read -p "At which update (multiple of 500) do you want to analyze the population? " ANALYSIS_TIME
+SLURM_ARRAY_TASK_ID=10
+ANALYSIS_TIME=$((1000 * SLURM_ARRAY_TASK_ID))
+echo $ANALYSIS_TIME
 
-python3 RunGeneDuplicationAvidaAnalysisScript.py ${EXPERIMENT_ID} ${ANALYSIS_TIME}
+cp RunGeneDuplicationAvidaAnalysisScript.py RunGeneDuplicationAvidaAnalysisScript_${ANALYSIS_TIME}.py
+python3 RunGeneDuplicationAvidaAnalysisScript_${ANALYSIS_TIME}.py ${EXPERIMENT_ID} ${ANALYSIS_TIME}
+rm RunGeneDuplicationAvidaAnalysisScript_${ANALYSIS_TIME}.py
 
-cd ${EXPERIMENT_DIR}
-
-module load gcc/11.2.0
-python3 CodingSiteGeneratorHPCCCopy.py ${ANALYSIS_TIME}
+rm geneDuplicationDataAnalyzer_${ANALYSIS_TIME}.sh
