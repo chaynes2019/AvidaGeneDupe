95f9e4dcc53285f14bc8209ecfa4125b820007ea
Cameron Haynes <chaynes2019@gmail.com>
2024-07-16 16:19:48 -0400

    Create runMutationMaskGeneration.sh to run generateMutationMasks.py over all Timepoints

---
+++ experiments/2022-3-29-PaperDuplication/AssociatedScripts/runMutationMaskGeneration.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# The interpreter used to execute the script
+#SBATCH --job-name=PaperDuplication-mutationMaskGeneration
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=clhaynes@umich.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=2g
+#SBATCH --time=00-08:00:00
+#SBATCH --account=zamanlh0
+#SBATCH --array=1-10
+
+USERNAME=clhaynes
+EXPERIMENT_ID=2022-3-29-PaperDuplication
+
+EXPERIMENT_DIR=/scratch/zamanlh_root/zamanlh0/${USERNAME}/${EXPERIMENT_ID}
+
+echo "Experiment: ${EXPERIMENT_ID}"
+SLURM_ARRAY_TASK_ID=10
+ANALYSIS_TIME=$((1000 * SLURM_ARRAY_TASK_ID))
+echo $ANALYSIS_TIME
+
+cp generateMutationMasks.py ${EXPERIMENT_DIR}/generateMutationMasks_${ANALYSIS_TIME}.py
+
+cd ${EXPERIMENT_DIR}
+
+python3 generateMutationMasks_${ANALYSIS_TIME}.py ${ANALYSIS_TIME}
+
+rm generateMutationMasks_${ANALYSIS_TIME}.py
